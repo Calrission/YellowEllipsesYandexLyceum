85d03cd01b2d943c275dd764549a7ca1d8fea6f7
Kavrese <va2005art@gmail.com>
2022-11-21 18:19:24 +0300

    next task

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from sys import argv, exit
 from PyQt5.QtGui import QPainter, QColor
 from random import randint
+from UI import Ui_MainWindow
 
 
-class Main(QMainWindow):
+class Main(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.need_paint = False
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.initUI()
 
     def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
         if self.need_paint:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 255, 153))
+            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             s = randint(5, 250)
             qp.drawEllipse(randint(0, self.width() - s), randint(0, 350 - s), s, s)
             qp.end()
